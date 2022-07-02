762a117342fa419b678d0dd2238f3298e31830b8
Jibran mohammad <jibran105@gmail.com>
2022-07-02 15:33:00 +0530

    finding maximum subarray using brute force in a file maxSubarrayBruteForece.py changed for better

--- Divide and Conquer/maxSubarrayBruteForce.py
+++ Divide and Conquer/maxSubarrayBruteForce.py
@@ -1,22 +1,38 @@
-
-
+"""
+Finding the maximum subarray using brute force: This algorithm takes O(n^2) time
+"""
 class MaximumSubarray:
-    def __init__(self, array):
-        self.array = array 
+    """
+    A class MaximumSubarray which contains __init__ method for creating attributes of instances and has maxsubarray 
+    method which applies brute force for finding all pair of indices of array, finding the sum and then finding
+    maximum among them.
+    """    
+    def __init__(self, array: list):
+        self.array = array
+        self.maxSum = array[0]
+        self.currentSum= 0
+        self.left= 0
+        self.right= 0 
 
     def maxSubarray(self):
         n = len(self.array)
-        maximumProfit = -100000000000000
         for i in range(n):
-            for j in range(i+1,n):
-                profit = self.array[j] - self.array[i]
-                if profit > maximumProfit:
-                    maximumProfit = profit 
-                    left = i 
-                    right = j
-        return left, right, maximumProfit
+            self.currentSum = 0
+            for j in range(i,n):
+                self.currentSum = self.currentSum + self.array[j]
+                if self.currentSum > self.maxSum:
+                    self.maxSum = self.currentSum 
+                    self.left = i 
+                    self.right = j
+
+    def __repr__(self):
+        return 'leftIndex= %d, rightIndex= %d, Sum = %d' % (self.left, self.right, self.maxSum)                
+
+def main():
+   instance = MaximumSubarray([1, -3, 4, -1, 2, 1, -5, 5])
+   instance.maxSubarray()
+   print(instance)         
       
 if __name__ == "__main__":
-    instance = MaximumSubarray([10,11,7,10,6])
-    left, right, profit = instance.maxSubarray()
-    print('buy date=', left, 'sell date=', right, 'profit=', profit)                    
+    main()
+                        
