a6c31a1cb6f2e683eeae180598b75e4bee704964
Jibran mohammad <jibran105@gmail.com>
2022-07-13 14:43:25 +0530

    Added the documentation to min priority Queue and max priority Queue

--- Elementary data structures/Priority Queues/maxPriorityQueue.py
+++ Elementary data structures/Priority Queues/maxPriorityQueue.py
@@ -1,30 +1,54 @@
-
-import sys
+"""
+Implementation of the max Priority Queue. It supports operations heapMax, heapIncreasekey, Insert and 
+extract max. This implementation uses maxHeap. heapMax will take O(1) time, heapIncreasekey will take
+O(logn) time, Insert will take O(logn) time and extract max will take O(logn) time.
+"""
 
 class MaxPriorityQueue:
+    """
+    class defined for max Priority Queue with two operating overloading methods and other methods for opearations
+    of maxpriorityQueue and some support methods like maxHeapify, parent, leftchild etc    
+    """
     def __init__(self, sequence):
         self.sequence = sequence
+        self.length= len(sequence)
         self.heapSize = 0
     
     def parent(self, i):
+        """
+        returns the parent of element at ith index
+        """
         return (i - 1) // 2
 
     def leftChild(self, i):
+        """
+        returns the left child of node at ith index
+        """
         return (2 * i) + 1
 
     def rightChild(self,i):
+        """
+        returns the right child of node at ith index
+        """
         return (2 * i) + 2
 
     def maxHeapify(self, i):
+        """
+        This method takes the index of the node as the argument and assumes the left index node and right index
+        node to be the the maxHeaps, but the element at ith location might be voilating the maxheap
+        property, so it moves it downwards in the tree to satisfy the maxheap property. If after moving it
+        one step downward, the new node voilates the maxheap property then the method will recurse and 
+        perform the same method again. This procedure takes O(logn) time in worst case.
+        """
         left = self.leftChild(i)
         right = self.rightChild(i)
 
-        if left < self.heapSize and self.sequence[left] > self.sequence[i]:
+        if left <= self.heapSize and self.sequence[left] > self.sequence[i]:
             largest = left
         else:
             largest = i 
 
-        if right < self.heapSize and self.sequence[right] > self.sequence[largest]:
+        if right <= self.heapSize and self.sequence[right] > self.sequence[largest]:
             largest = right
 
         if largest != i:
@@ -32,60 +56,83 @@ class MaxPriorityQueue:
             self.maxHeapify(largest)
 
     def buildHeap(self):
-        self.heapSize = len(self.sequence)
-        for i in range((len(self.sequence) // 2) -1, -1, -1):
+        """
+        Convert the array which isn't a maxheap into the maxheap. The loop starts from the last nonleaf node
+        and starts running maxheapify procedure on the element. This procedure takes O(n) time.
+        """
+        self.heapSize = self.length - 1
+        for i in range((self.length // 2) -1, -1, -1):
             self.maxHeapify(i) 
 
     def heapMax(self):
+        """
+        return the maximum priority element in the heap
+        """
         return self.sequence[0]
 
     def heapExtractMax(self):
+        """
+        return and deletes the maximum priority element in the heap
+        """
         if self.heapSize < 0:
-            print('No element present in the heap')
-
-        max = self.sequence[0]
-        self.sequence[0] = self.sequence[self.heapSize - 1]
-        self.heapSize = self.heapSize - 1
-        self.maxHeapify(0) 
-        return max 
+            return 'No element present in the heap'
+        else:
+            max = self.sequence[0]
+            self.sequence[0] = self.sequence[self.heapSize]
+            self.heapSize = self.heapSize - 1
+            self.maxHeapify(0) 
+            return max 
 
     def heapIncreaseKey(self, i, key):
+        """
+        Increases the element by the given value
+        """
         if key < self.sequence[i]:
-            print('New key is smaller than the current value')
-            return
-        self.sequence[i] = key 
+            return 1
+        else:  
+            self.sequence[i] = key 
 
-        while i > 0 and self.sequence[i] >= self.sequence[self.parent(i)]:
-            self.sequence[i], self.sequence[self.parent(i)] = self.sequence[self.parent(i)], self.sequence[i]
-            i = self.parent(i)
+            while i > 0 and self.sequence[i] >= self.sequence[self.parent(i)]:
+                self.sequence[i], self.sequence[self.parent(i)] = self.sequence[self.parent(i)], self.sequence[i]
+                i = self.parent(i)
 
-    def maxHeapInsert(self, key):
-        minsize = -sys.maxsize - 1
+    def insert(self, key):
+        """
+        Inserts the element inside the max Priority Queue
+        """
+        minsize = float('-inf')
         self.heapSize += 1
         self.sequence.append(minsize)
-        self.heapIncreaseKey(self.heapSize - 1, key)
+        self.heapIncreaseKey(self.heapSize, key)
     
     def __str__(self):
         tempList = []
-        i = 0
-        for x in self.sequence:
-            if i < self.heapSize:
-                tempList.append(x)
-                i += 1
-        return 'Sequence is: %s' % tempList
+        for i in range(0,self.heapSize + 1):
+            tempList.append(self.sequence[i])
+        tempList = [str(x) for x in tempList]
+        return ' '.join(tempList)
 
-
-if __name__ == '__main__':
-    instance = MaxPriorityQueue([15,13,9,5,12,8,7,4,0,6,2,1])
+def main():
+    instance = MaxPriorityQueue([4,1,3,2,16,9,10,14,8,7])
     instance.buildHeap()
-    maximum = instance.heapExtractMax()
-    print('The maximum number extracted is:', maximum)
-    print('After extracting max, the resulted sequence is')
     print(instance)
+    maximum = instance.heapExtractMax()
+    if isinstance(maximum, str):
+        print('No element present in the heap')
+    else:    
+        print('The maximum number extracted is:', maximum)
+        print('After extracting max, the resulted sequence is')
+        print(instance)
     maximum = instance.heapMax()
     print('The maximum element in the heap is: ', maximum)
-    instance.heapIncreaseKey(3, 100)
-    print('The sequence after key at location 3 has been incresed is:', instance)
-    instance.maxHeapInsert(25)
+    flag = instance.heapIncreaseKey(3, 100)
+    if flag ==1:
+        print('New key is smaller than the original key')
+    else:    
+        print('The sequence after key at location 3 has been increased is:', instance)
+    instance.insert(25)
     print('The sequence after insertion took place is: ', instance)
+
+if __name__ == '__main__':
+    main()
 
--- Elementary data structures/Priority Queues/minPriorityQueue.py
+++ Elementary data structures/Priority Queues/minPriorityQueue.py
@@ -1,29 +1,54 @@
+"""
+Implementation of the min Priority Queue. It supports operations heapMin, heapdecreasekey, Insert and 
+extract min. This implementation uses minHeap. heapMin will take O(1) time, heapdecreasekey will take
+O(logn) time, Insert will take O(logn) time and extract min will take O(logn) time.
+"""
 
-import sys
 class MinPriorityQueue:
+    """
+    class defined for min Priority Queue with two operating overloading methods and other methods for opearations
+    of maipriorityQueue and some support methods like minHeapify, parent, leftchild etc    
+    """
     def __init__(self, sequence):
         self.sequence = sequence
         self.heapSize = 0
+        self.length = len(sequence)
 
     def leftChild(self, i):
+        """
+        returns the left child of node at ith index
+        """
         return (2 * i) + 1
 
     def rightChild(self, i):
+        """
+        returns the right child of node at ith index
+        """
         return (2 * i) + 2
 
     def parent(self, i):
+        """
+        returns the parent of element at ith index
+        """
         return ((i - 1) // 2)
     
     def minHeapify(self,i):
+        """
+        This method takes the index of the node as the argument and assumes the left index node and right index
+        node to be the the minHeaps, but the element at ith location might be voilating the minheap
+        property, so it moves it downwards in the tree to satisfy the minheap property. If after moving it
+        one step downward, the new node voilates the minheap property then the method will recurse and 
+        perform the same method again. This procedure takes O(logn) time in worst case.
+        """
         left = self.leftChild(i)
         right = self.rightChild(i)
 
-        if left < self.heapSize and self.sequence[left] < self.sequence[i]:
+        if left <= self.heapSize and self.sequence[left] < self.sequence[i]:
            smallest = left 
         else:
             smallest = i 
 
-        if right < self.heapSize and self.sequence[right] < self.sequence[smallest]:
+        if right <= self.heapSize and self.sequence[right] < self.sequence[smallest]:
             smallest = right 
 
         if smallest != i:
@@ -31,50 +56,73 @@ class MinPriorityQueue:
             self.minHeapify(smallest)                      
     
     def buildMinHeap(self):
-        self.heapSize = len(self.sequence)
-        n = len(self.sequence)
-        for j in range((n // 2) - 1, -1, -1):
+        """
+        Convert the array which isn't a minheap into the minheap. The loop starts from the last nonleaf node
+        and starts running minheapify procedure on the element. This procedure takes O(n) time.
+        """
+        self.heapSize = self.length - 1
+        for j in range((self.length // 2) - 1, -1, -1):
             self.minHeapify(j)
 
     def __str__(self):
         tempList = []
-        j = 0
-        for x in self.sequence:
-            if j < self.heapSize:
-                tempList.append(x)
-                j += 1
-        return 'Sequence is: %s' % tempList
+        for index in range(0, self.heapSize + 1):
+                tempList.append(self.sequence[index])
+        tempList = [str(x) for x in tempList]        
+        return ' '.join(tempList)
 
     def extractMin(self):
-        minimum = self.sequence[0]
-        self.sequence[0] = self.sequence[self.heapSize - 1]
-        self.heapSize -= 1
-
-        self.minHeapify(0)
+        """
+        return and deletes the minimum priority element in the heap
+        """
+        if self.heapSize < 0:
+            return 'No element present in the heap'
+        else:    
+            minimum = self.sequence[0]
+            self.sequence[0] = self.sequence[self.heapSize]
+            self.heapSize -= 1
+            self.minHeapify(0)
+            return minimum
 
     def minDecreaseKey(self, i, key):
+        """
+        Decreases the element by the given value
+        """
         if key > self.sequence[i]:
-            print('Key given is larger than the key present')
-            return 
-        
-        self.sequence[i] = key 
-        while i > 0 and self.sequence[i] < self.sequence[self.parent(i)]:
-            self.sequence[self.parent(i)], self.sequence[i] = self.sequence[i], self.sequence[self.parent(i)]
-            i = self.parent(i)  
+            return 1
+        else:
+            self.sequence[i] = key 
+            while i > 0 and self.sequence[i] < self.sequence[self.parent(i)]:
+                self.sequence[self.parent(i)], self.sequence[i] = self.sequence[i], self.sequence[self.parent(i)]
+                i = self.parent(i)  
     
     def minHeapInsert(self, key):
+        """
+        Inserts the element inside the max Priority Queue
+        """
         self.heapSize = self.heapSize + 1
-        self.sequence.append(sys.maxsize)
-        self.sequence[self.heapSize]= sys.maxsize
+        self.sequence.append(float('inf'))
         self.minDecreaseKey(self.heapSize, key)
 
-if __name__=='__main__':
-    instance = MinPriorityQueue([15,13,9,5,12,8,7,4,0,6,2,1])
+def main():
+    instance = MinPriorityQueue([4,1,3,2,16,9,10,14,8,7])
     instance.buildMinHeap()
     print(instance)
-    instance.extractMin()
-    print(instance)
-    instance.minDecreaseKey(5,0)
-    print(instance)
+    minimum = instance.extractMin()
+    if isinstance(minimum, str):
+        print(minimum)
+    else:
+        print('The min Queue after deleting min element is ')
+        print(instance)
+    flag = instance.minDecreaseKey(5,0)
+    if flag == 1:
+        print('The value you want to insert is greater than the value already present at node')
+    else: 
+        print('The min Priority Queue after decreasing a key by certain value is ')   
+        print(instance)
     instance.minHeapInsert(6)
+    print("The min Priority Queue after inserting a key is")
     print(instance)
+
+if __name__=='__main__':
+   main() 
